57787405ece4822b9fec2f74f23d73d0b32f5d9c
Canhui <320653941@qq.com>
2022-12-13 11:14:40 +0800

    touch a new file

---
+++ exp5_orderer9/scp.sh
@@ -0,0 +1,108 @@
+#--------------------------------------------------------------------------------------------------
+# The first time takes a few minutes
+#--------------------------------------------------------------------------------------------------
+# The demo-frist folder
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.102:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.132:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.134:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.138:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.140:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.141:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.142:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.151:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.152:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.153:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.154:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.155:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.156:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.121:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.123:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.124:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.125:$HOME/fabric-samples/
+#scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.126:$HOME/fabric-samples/
+#scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.127:$HOME/fabric-samples/
+#scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.131:$HOME/fabric-samples/
+#scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.132:$HOME/fabric-samples/
+#scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.135:$HOME/fabric-samples/
+#scp -r $HOME/fabric-samples/demo-first/ $USER@192.168.0.136:$HOME/fabric-samples/
+
+
+# The chaincode folder
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.102:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.132:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.134:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.138:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.140:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.141:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.142:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.151:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.152:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.153:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.154:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.155:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.156:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.121:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.123:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.124:$HOME/fabric-samples/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.125:$HOME/fabric-samples/abstore/go
+#scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.126:$HOME/fabric-samples/abstore/go
+#scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.127:$HOME/fabric-samples/abstore/go
+#scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.131:$HOME/fabric-samples/abstore/go
+#scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.132:$HOME/fabric-samples/abstore/go
+#scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.135:$HOME/fabric-samples/abstore/go
+#scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@192.168.0.136:$HOME/fabric-samples/abstore/go
+
+
+#--------------------------------------------------------------------------------------------------
+# Copy config file
+#--------------------------------------------------------------------------------------------------
+# The config file
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.102:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.132:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.134:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.138:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.140:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.141:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.142:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.151:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.152:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.153:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.154:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.155:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.156:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.121:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.123:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.124:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.125:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.126:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.127:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.131:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.132:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.135:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@192.168.0.136:$HOME/fabric-samples/demo-first/
+
+
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.102:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.132:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.134:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.138:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.140:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.141:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.142:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.151:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.152:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.153:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.154:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.155:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.156:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.121:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.123:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.124:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.125:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.126:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.127:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.131:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.132:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.135:$HOME/fabric-samples/demo-first/
+#scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@192.168.0.136:$HOME/fabric-samples/demo-first/
+
